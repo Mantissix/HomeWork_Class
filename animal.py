b63dd2a5dbb485d10f8427dcf3cadb558d086b19
Mantissix <437897595@qq.com>
2021-08-10 10:37:39 +0800

    第七次修改，添加文件搜索

--- animal.py
+++ animal.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 
 class animals:
@@ -36,11 +37,16 @@ class dog(animals):
 
     def saveDog(self):
         # 关于yaml.safe_dump补充：设置allow_unicode=True,可以将中文写入文件；设置sort_keys=False,则不会在排序后写入，如果要追加写入，则将读取方式由'w'改为'a'
+        # 关于open函数，如果要打开的文件对象不存在，调用open函数时会自动在指定目录下创建一个该对象，如果存在，则直接调用
         dogProperty = {self.name: {'varietie': self.varietie, 'color': self.color, 'age': self.age, 'gender': self.gender, 'dogFur': self.dogFur, 'skill': self.skill}}
-        with open('./datas/dogData', 'a', encoding='UTF-8') as dog1:
-            yaml.safe_dump(dogProperty, dog1, allow_unicode=True, sort_keys=False)
-        print('狗狗数据已保存')
-
+        if not os.path.exists('./datas/dogData'):
+            with open('./datas/dogData', 'a', encoding='UTF-8') as dog1:
+                yaml.safe_dump(dogProperty, dog1, allow_unicode=True, sort_keys=False)
+            print('本地文件创建成功，狗狗数据已保存')
+        else:
+            with open('./datas/dogData', 'a', encoding='UTF-8') as dog1:
+                yaml.safe_dump(dogProperty, dog1, allow_unicode=True, sort_keys=False)
+            print('狗狗数据已保存')
 
 class cat(animals):
     catFur: str
@@ -54,6 +60,11 @@ class cat(animals):
 
     def saveCat(self):
         catProperty = {self.name: {'varietie': self.varietie, 'color': self.color, 'age': self.age, 'gender': self.gender, 'dogFur': self.catFur, 'skill': self.skill}}
-        with open('./datas/catData', 'a', encoding='UTF-8') as cat1:
-            yaml.safe_dump(catProperty, cat1, allow_unicode=True, sort_keys=False)
-        print('猫咪数据已保存')
+        if not os.path.exists('./datas/catData'):
+            with open('./datas/catData', 'a', encoding='UTF-8') as cat1:
+                yaml.safe_dump(catProperty, cat1, allow_unicode=True, sort_keys=False)
+            print('本地文件创建成功，猫咪数据已保存')
+        else:
+            with open('./datas/catData', 'a', encoding='UTF-8') as cat1:
+                yaml.safe_dump(catProperty, cat1, allow_unicode=True, sort_keys=False)
+            print('猫咪数据已保存')
